40a68418f76d21c06bbb6296b5a38247a5485b94
Mac <mac@Macs-MacBook-Pro.local>
2024-10-16 15:16:11 +0300

    arrays and slices

--- test.go
+++ test.go
@@ -3,32 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	age := 22
-	name := "Lewis"
-
 	/*
-	    imported fmt use,
-	   1.Print function
+		Arrays
+		len is length
+		start by variable, variable name, then length of the array, followed by variable type
 	*/
-	fmt.Println("Hello", name, "you are", age)
+	var ages [3]int = [3]int{25, 57, 58}
 
-	//formatted string
-	/*format specifiers (
-	%v - default format for variables(strings,integers,boolean etc)
-	%s - format for strings
-	%d - format for integers
-	%f - format for float
-	%T - format for type of variable
+	names := [4]string{"Silus", "Gibson", "Lewis", "Lebron"}
+	names[1] = "gina" //this replaces the number 1 value from "Gibson" to "Gina"
 
-	)
-	*/
-	fmt.Printf("My name is %v and my age is %v", name, age)
-	fmt.Printf("age is of %T", age)
+	fmt.Println(ages, len(ages)) //the len prints length of array
+	fmt.Println(names, len(names))
 
-	//Sprintf
+	//Slices
 	/*
+		we dont enter number in square brackets
+	*/
+	var scores = []int{100, 50, 60} //this slice makes it easy to extend number of items on array without limit
+	scores[2] = 25
+	scores = append(scores, 85) //appends element to end of slice, making it remove 60 and add 85
 
-	 */
-	var str = fmt.Sprintf("My age is %v and my name is %v\n", age, name)
-	fmt.Println(str)
+	fmt.Println(scores, len(scores))
+
+	//slice ranges
+	rangeOne := names[1:3]  //get me from position 1 to 3
+	rangeTwo := names[2:]   //get me from position 2 to 3
+	rangeThree := names[:3] //get upto position 3
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+	rangeOne = append(rangeOne, "Beatrice")
+	fmt.Println(rangeOne)
 }
