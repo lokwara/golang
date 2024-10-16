d74f5aa54ae0dc4a6cec220382f187a4acf424bd
Mac <mac@Macs-MacBook-Pro.local>
2024-10-16 14:28:03 +0300

    variables and integers

--- test.go
+++ test.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Lewis")
+	// strings
+	var nameOne string = "Lewis"
+	//integer
+	var age int = 22
+	//float
+	var deci float32 = 2.2
+	var dec float64 = 2.5
+	//boolean
+	var yes bool = false
+
+	// another way of declaring variables.
+	// You dont have to give variable type and var
+	// cant be used out of a function
+	nameTwo := "Lewis Okoche"
+	ages := 22
+	decis := 3.142
+	nooo := true
+
+	fmt.Println(nameOne, age, deci, yes, dec, nameTwo, ages, decis, nooo)
 }
