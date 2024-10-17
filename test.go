de5fe32765e012d2010c22fb0895c33025b66085
Mac <mac@Macs-MacBook-Pro.local>
2024-10-17 12:16:52 +0300

    loops in go

--- test.go
+++ test.go
@@ -2,34 +2,33 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	/*
-		Standard Library
+	/*x := 0
+	for x < 5 {
+		fmt.Println("value of x is:", x)
+		x++
+	}
 	*/
-	//greeting := "Hello Friends"
 
-	//fmt.Println(strings.Contains(greeting, "hello"))
-	//fmt.Println(strings.ReplaceAll(greeting, "Hello", "hi")) //subtituted the hello with hi on output
+	//for i := 0; i < 5; i++ {
+	//	fmt.Println("value of i is:", i)
+	//}
 
-	//fmt.Println(strings.Index(greeting, "ll"))
-	//fmt.Println(strings.Split(greeting, " "))
-	//fmt.Println(strings.ToUpper(greeting))
+	names := []string{"Lewis", "Lebron", "Gibson", "Silus"}
 
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+	//for i := 0; i < len(names); i++ {
+	//	fmt.Println(names[i])
+	//}
+	//for index, value := range names {
+	//	fmt.Printf("The position at index %v is %v \n", index, value)
+	//}
 
-	sort.Ints(ages) //method 'sort' sorts the integers from small to big
-	fmt.Println(ages)
+	for _, value := range names {
+		fmt.Printf("The value is %v \n", value)
+		value = "new string"
+	}
 
-	index := sort.SearchInts(ages, 75) //searches and gives you where it is located
-	fmt.Println(index)
-
-	names := []string{"Lewis", "Lebron", "Beatrice", "Gibson", "Silus"}
-
-	sort.Strings(names) //this method sorted names according to name and letter
 	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "Beatrice"))
 }
