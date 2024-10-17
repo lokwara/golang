72175eeb4370092dec57c667b81af93b6130a21d
Mac <mac@Macs-MacBook-Pro.local>
2024-10-17 13:43:42 +0300

    package scope

--- test.go
+++ test.go
@@ -2,33 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
+var score = 99.5
 
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-
-}
-
+// make multiple files in a package
 func main() {
-	fn1, sn1 := getInitials("lewis okwara")
-	fmt.Println(fn1, sn1)
 
-	fn2, sn2 := getInitials("Gibson okwara")
-	fmt.Println(fn2, sn2)
+	sayHello("Lewis")
 
-	fn3, sn3 := getInitials("okwara")
-	fmt.Println(fn3, sn3)
+	for _, v := range points {
+		fmt.Println(v)
+	}
+	showScore()
 }
