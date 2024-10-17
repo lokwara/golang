7801864af86d1846cb00a0998e87c7f5c62af4bf
Mac <mac@Macs-MacBook-Pro.local>
2024-10-17 14:32:27 +0300

    Pass by value

--- test.go
+++ test.go
@@ -4,41 +4,32 @@ import (
 	"fmt"
 )
 
-// Maps
+// Go is a Pass-by-value Language
+
+func updateName(x string) string {
+	x = "wedge"
+	return x
+}
+
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
+}
+
 func main() {
+	// group A types -> strings, ints, bools, arrays, structs
+	name := "Lewis"
 
+	name = updateName(name)
+
+	fmt.Println(name)
+
+	//group B types -> slices, maps, functions
 	menu := map[string]float64{
-		"soup":      4.99,
-		"Ugali":     2.99,
-		"Sarat":     1.99,
-		"Misheveve": 0.99,
+		"pie":       5.95,
+		"ice cream": 3.99,
 	}
 
+	updateMenu(menu)
 	fmt.Println(menu)
-	fmt.Println(menu["pie"])
-	//Maps entire menu in the map
-	//start with  map variable variable
-
-	//looping maps
-	//k,key is "soup", v,value is "4.99"
-	for k, v := range menu {
-
-		fmt.Println(k, "_", v)
-	}
-
-	//int as key type
-	phonebook := map[int]string{
-		769731531: "Lewis",
-		710211186: "Mum",
-		725797511: "Dad",
-	}
-	fmt.Println(phonebook)
-	fmt.Println(phonebook[769731531])
-
-	phonebook[725797511] = "Silus"
-	fmt.Println(phonebook)
-
-	phonebook[710211186] = "Beatrice"
-	fmt.Println(phonebook)
 
 }
