1e52af02760cf24da4c1cbdcf17761bb65cd0b6c
Mac <mac@Macs-MacBook-Pro.local>
2024-10-17 14:06:28 +0300

    Maps

--- test.go
+++ test.go
@@ -4,15 +4,41 @@ import (
 	"fmt"
 )
 
-var score = 99.5
-
-// make multiple files in a package
+// Maps
 func main() {
 
-	sayHello("Lewis")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":      4.99,
+		"Ugali":     2.99,
+		"Sarat":     1.99,
+		"Misheveve": 0.99,
 	}
-	showScore()
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+	//Maps entire menu in the map
+	//start with  map variable variable
+
+	//looping maps
+	//k,key is "soup", v,value is "4.99"
+	for k, v := range menu {
+
+		fmt.Println(k, "_", v)
+	}
+
+	//int as key type
+	phonebook := map[int]string{
+		769731531: "Lewis",
+		710211186: "Mum",
+		725797511: "Dad",
+	}
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[769731531])
+
+	phonebook[725797511] = "Silus"
+	fmt.Println(phonebook)
+
+	phonebook[710211186] = "Beatrice"
+	fmt.Println(phonebook)
+
 }
